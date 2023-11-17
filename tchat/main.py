b82ce88bff5389df7fddaa773f6ba4c8a6924939
Mahto, Vivek Kumar <Vivek.Mahto@uni-bayreuth.de>
2023-11-17 23:16:02 +0100

    Adding more to chat: <comments and working>

--- tchat/main.py
+++ tchat/main.py
@@ -1,6 +1,13 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import (
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.memory import (
+    ConversationBufferMemory,
+)  # To store conversation messages in the buffer
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,15 +33,39 @@ load_dotenv()
 
 chat = ChatOpenAI()
 
+"""
+    Memory => Is an important class in Langchain, that can be associated with a chain
+    to hold information in that chain, the chain will use that memory whenever the chain is used
+    
+    ConversationBufferMemory(memory_key="messages", return_messages=True)
+        "memory_key"=> All the previous and current messages will be stored here
+        "return_messages=True"=> This means that messages are stored in the form of
+                Human: <message>
+                Assistant: <message>
+"""
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
-    messages=[HumanMessagePromptTemplate.from_template("{content}")],
+    input_variables=["content", "messages"],
+    # current message(content) and old messages combined
+    messages=[
+        MessagesPlaceholder(
+            variable_name="messages"
+        ),  # IMP to pass the conversation history
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
 )
 
-chain = LLMChain(llm=chat, prompt=prompt)
+"""
+    Once the memory is associated with the chain
+    The 'content' and 'response' are automatically appended to the "messages"
+"""
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
 
 while True:
     content = input(">>")
-    result = chain({"content": content})
+    result = chain(
+        {"content": content}
+    )  # The result is automatically appended to the memory in chain
 
     print(result["text"])
